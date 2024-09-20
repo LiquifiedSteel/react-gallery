3d34f88ab5f95620d651a4e25c2ae743067f0c74
Alex Rivard <alex.rivard4406@gmail.com>
2024-09-20 17:04:59 -0500

    when the gallery items are clicked on, they toggle between showing the picture and the picture description

--- src/components/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import './GalleryItem.css';
+import { useState } from 'react';
 
 function GalleryItem({image, fetchGallery}) {
+    const [visible, setVisible] = useState(true)
     function addLike(image) {
         axios({
             method: "PUT",
@@ -14,8 +16,9 @@ function GalleryItem({image, fetchGallery}) {
 
     return (
         <div data-testid="galleryItem">
-            <img className="image" src={image.url}/>
-            <button onClick={() => addLike(image.id)}>👍🏻</button>
+            <h3>{image.title}</h3>
+            {visible ? <img className="image" onClick={() => setVisible(!visible)} src={image.url}/> : <h4 onClick={() => setVisible(!visible)}>{image.description}</h4>}
+            <button data-testid="like" onClick={() => addLike(image.id)}>👍🏻</button>
             <p>{image.likes===0? 'No' : image.likes} {image.likes===1? 'person loves' : 'people love'} this!</p>
         </div>
     )
