113d7288af19ba0bc67409e857706aeb320f7cbf
Alex Rivard <alex.rivard4406@gmail.com>
2024-09-20 16:52:52 -0500

    gallery is displayed on DOM with a button to add a like, and a statement telling how many likes the image has

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,7 +4,15 @@ const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  let reqId = req.params.id;
+    console.log("In PUT purchase route");
+    const query = `UPDATE gallery SET "likes"=("likes"+1) WHERE "id" = $1;`;
+    pool.query(query, [reqId]).then((results) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error making PUT', error);
+        res.sendStatus(500);
+    });
 });
 
 // GET route to get all the images from the database
--- src/components/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,22 @@
-
-
+import axios from 'axios';
+import './GalleryItem.css';
 
 function GalleryItem({image, fetchGallery}) {
+    function addLike(image) {
+        axios({
+            method: "PUT",
+            url: `/api/gallery/like/${image}`
+        }).then(response => {
+            console.log('adding a like', image.id);
+            fetchGallery();
+        }).catch(err => console.error('error adding a like', err));
+    }
 
     return (
         <div data-testid="galleryItem">
-            <img src={image.url}/>
+            <img className="image" src={image.url}/>
+            <button onClick={() => addLike(image.id)}>👍🏻</button>
+            <p>{image.likes===0? 'No' : image.likes} {image.likes===1? 'person loves' : 'people love'} this!</p>
         </div>
     )
 }
