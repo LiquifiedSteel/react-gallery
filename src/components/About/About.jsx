9e4ce10044b57ab40ae450b3b8ba2339d7866713
Alex Rivard <alex.rivard4406@gmail.com>
2024-09-23 16:57:16 -0500

    errors regarding routing are gone, somehow

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+
+
+function About() {
+    
+    return (
+        <h3>This site is used to display some wonderful images.</h3>
+    )
+}
+
+export default About;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 // Dependencies
 import React from 'react';
 import GalleryList from '../GalleryList/GalleryList';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import About from '../About/About';
 
 // Styles
 import './App.css';
@@ -9,16 +11,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
-    <div data-testid="app">
-      <div className='jumbotron'>
-        <header>
-          <h1>Alex's Gallery</h1>
-        </header>
+    <>
+      <div data-testid="app">
+        <div className='jumbotron'>
+          <header>
+            <h1>Alex's Gallery</h1>
+          </header>
+        </div>
+        <Router>
+            <Route path="/" exact>
+              <GalleryList />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+        </Router>
       </div>
-      
-
-      <GalleryList />
-    </div>
+    </>
   );
 }
 
